fdc6ab625f73aa3bd2dd4d8100cb3390ec1bf1e4
Farzan Mirza <farzanmrz@gmail.com>
2025-06-18 11:21:37 -0400

    Commit with quoted message

--- gitquick_full_test.sh
+++ gitquick_full_test.sh
@@ -2,29 +2,29 @@
 
 echo "\n--- Test 1: Quoted commit message ---"
 echo "edit1" >> gitquick_dummy.txt
-gitquick 'Commit with quoted message'
-sleep 0.5
+gitq 'Commit with quoted message'
+sleep 2
 
 echo "\n--- Test 2: Bareword unquoted message ---"
 echo "edit2" >> gitquick_dummy.txt
-gitquick Hello123
-sleep 0.5
+gitq Hello123
+sleep 2
 
 echo "\n--- Test 3: No message (fallback to datetime) ---"
 echo "edit3" >> gitquick_dummy.txt
-gitquick
-sleep 0.5
+gitq
+sleep 2
 
 echo "\n--- Test 4: Too many arguments ---"
-gitquick one two
-sleep 0.5
+gitq one two
+sleep 2
 
 echo "\n--- Test 5: Push fails (no upstream) ---"
 git checkout -b testbranch-no-upstream
 echo "edit4" >> gitquick_dummy.txt
-gitquick 'Trying push with no upstream'
-sleep 0.5
+gitq 'Trying push with no upstream'
+sleep 2
 
-echo "\n--- Cleaning up test brfanch ---"
+echo "\n--- Cleaning up test branch ---"
 git checkout main
 git branch -D testbranch-no-upstream
