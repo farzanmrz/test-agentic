d157af941c5178f14804e12240c52fd5a607dd7d
Farzan Mirza <farzanmrz@gmail.com>
2025-06-25 02:04:52 -0700

    #2 init fastapi skeleton with ping route

---
+++ backend/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def ping():
+    return JSONResponse(content={"status": "ok"})
